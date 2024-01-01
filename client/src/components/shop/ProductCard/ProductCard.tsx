8086ef16620f0b0c122f1d1faea94eae50f27cf3
wjs_5025 <wjs_5025@naver.com>
2024-01-02 00:23:29 +0900

    feat: product 관련 컴포넌트 구현

--- client/src/components/shop/ProductCard/ProductCard.tsx
+++ client/src/components/shop/ProductCard/ProductCard.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './style.css';
+import { ReactComponent as Cart } from 'assets/icons/cart.svg';
 import { IProduct } from 'types/product';
 import getDiscountPrice from 'utils/getDiscountPrice';
 import getLocalePrice from 'utils/getLocalePrice';
 import getStorageMethodClassName from 'utils/product/getStorageMethodClassName';
 import getFormattedProductIdx from 'utils/product/getFormattedProductIdx';
+import { addProductToCartApi } from 'api/cart';
+import useCart from 'hooks/useCart';
+import { useNavigate } from 'react-router-dom';
 
 interface IProductCardProps {
 	idx: number;
@@ -12,10 +16,26 @@ interface IProductCardProps {
 }
 
 function ProductCard(props: IProductCardProps) {
+	const { refetch } = useCart();
+	const navigate = useNavigate();
 	const { idx, product } = props;
 
+	const add = async () => {
+		const response = await addProductToCartApi(product.productId);
+		if (response.status === 200) {
+			refetch();
+			if (window.confirm('장바구니에 상품이 추가되었습니다! 장바구니로 이동하시겠어요?')) {
+				navigate('/cart');
+			}
+		}
+	};
+
 	return (
-		<div className="product-card-container">
+		<div role="presentation" className="product-card-container" onClick={add}>
+			<div className="overlay">
+				<Cart />
+				<span>장바구니에 추가</span>
+			</div>
 			<span className="idx">{getFormattedProductIdx(idx)}</span>
 			<img className="photo" src={product.photoUrl} alt="product img" />
 			<p className="name">{product.name}</p>
@@ -24,10 +44,14 @@ function ProductCard(props: IProductCardProps) {
 				<span className="final-price">
 					<b>{getLocalePrice(getDiscountPrice(product.price, product.discountRate))}</b>원
 				</span>
-				<span className="original-price">
-					<s>{getLocalePrice(product.price)}원</s>
-				</span>
-				<span className="discount-rate">{product.discountRate}%</span>
+				{product.discountRate !== 0 && (
+					<>
+						<span className="original-price">
+							<s>{getLocalePrice(product.price)}원</s>
+						</span>
+						<span className="discount-rate">{product.discountRate}%</span>
+					</>
+				)}
 			</div>
 			<p className={`storage-method ${getStorageMethodClassName(product.storageMethod)}`}>{product.storageMethod}</p>
 		</div>
--- client/src/components/shop/ProductList/ProductList.tsx
+++ client/src/components/shop/ProductList/ProductList.tsx
@@ -1,28 +1,24 @@
+/* eslint-disable react/no-array-index-key */
 import React, { useEffect, useState } from 'react';
 
 import './style.css';
-import axios from 'axios';
 import { IProduct } from 'types/product';
+import useProduct from 'hooks/useProduct';
 import ProductCard from '../ProductCard/ProductCard';
 
 function ProductList() {
-	const [products, setProducts] = useState([]);
-
-	const getData = async () => {
-		const res = await axios.get('api/products');
-		if (res.status === 200) {
-			console.log(res);
-			setProducts(res.data);
-		}
-	};
+	const { data } = useProduct();
+	const [products, setProducts] = useState<IProduct[]>([]);
 
 	useEffect(() => {
-		getData();
-	}, []);
+		if (data?.status === 200) {
+			setProducts(data.data);
+		}
+	}, [data]);
 
 	return (
 		<section className="product-list">
-			{products.length && products.map((el: IProduct, idx) => <ProductCard key={el.id} idx={idx + 1} product={el} />)}
+			{products.length && products.map((el: IProduct, idx) => <ProductCard key={idx} idx={idx + 1} product={el} />)}
 		</section>
 	);
 }
